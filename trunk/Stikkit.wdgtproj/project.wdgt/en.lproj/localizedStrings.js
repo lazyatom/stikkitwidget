d066f9f3ae7c374e44d37f1c4d0e3dc6d7e3eccd
james.adam <james.adam@84222fcb-3b2d-0410-b2d7-a941e2e03da7>
2007-07-08 21:48:08 +0000

    Adding initial revision

---
+++ trunk/Stikkit.wdgtproj/project.wdgt/en.lproj/localizedStrings.js
@@ -0,0 +1,5 @@
+var localizedStrings = new Object;
+
+localizedStrings['status'] = 'Please Wait...';
+localizedStrings['James Adam'] = 'Hastily knocked together by James Adam';
+localizedStrings['queries'] = 'james@lazyatom.com :: interblah.net';
